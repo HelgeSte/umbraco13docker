a0e3e7dfa2f6b7ff8bd42a8ff1742d889a22fb3c
Helge Stegemoen <helge@stegemoen.com>
2025-02-06 23:03:34 +0100

    Project with connection string to Docker SQL server

---
+++ umb13-webwonders-testing/Program.cs
@@ -0,0 +1,32 @@
+using Webwonders.Umbraco.DockerConfiguration;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+// Enable Docker-based SQL Server configuration
+DockerConfigurationMiddleware.ConfigureDockerSqlDb(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
